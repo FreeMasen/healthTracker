1a75de74c7dd170a104c262f50f02326c730ed90
rfm <r@robertmasen.pizza>
2019-07-08 19:43:37 -0500

    publish

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 ng build --prod --output-path docs --base-href /healthTracker/
 ./node_modules/.bin/webpack
-echo "just say no to jekyll\n" > ./docs/.nojekyll
+echo "just say no to jekyll\
+" > ./docs/.nojekyll
 cp ./docs/index.html ./docs/404.html
 git add . && git commit -m "publish" && git push
